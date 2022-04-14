9fe2d8da7ea97476a360fa8d30222570bd6b3140
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-04-14 00:44:39 -0300

    feat: 🎸 Queries

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -15,16 +15,28 @@ export class GamesRepository implements IGamesRepository {
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
       .createQueryBuilder()
+      .select()
+      .from(Game,'games')
+      .where('games.title ILIKE :param', { param: param})
+      .getMany();
+
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT COUNT(*) FROM games`); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
       .createQueryBuilder()
+      .select()
+      .from(User,'users')
+      .innerJoin('users.games', 'games')
+      .where('games.id = :id', { id: id})
+      .getMany();
+      
+
       // Complete usando query builder
   }
 }
